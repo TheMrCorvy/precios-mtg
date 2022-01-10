475a56a45e8846dab7550b8d98e207749d80998b
Gonzalo Salvador Corvalan <mr.corvy@gmail.com>
2022-01-10 13:59:13 -0300

    Updates

---
+++ precache-manifest.9b9607905e4e7bc7575c59f9d418aa78.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3d4a3847db822558ec64d3af8a62d978",
+    "url": "/precios-mtg/index.html"
+  },
+  {
+    "revision": "38fa774836e017177434",
+    "url": "/precios-mtg/static/css/main.6669b886.chunk.css"
+  },
+  {
+    "revision": "93e4cb42135c4776d800",
+    "url": "/precios-mtg/static/js/2.71673d91.chunk.js"
+  },
+  {
+    "revision": "d2966845b94a3318bf32eecc7af8015d",
+    "url": "/precios-mtg/static/js/2.71673d91.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "38fa774836e017177434",
+    "url": "/precios-mtg/static/js/main.a2d32aab.chunk.js"
+  },
+  {
+    "revision": "9b3373fae83c351165f6",
+    "url": "/precios-mtg/static/js/runtime-main.7b2c5ab0.js"
+  },
+  {
+    "revision": "b81dabc0bc6ca52fce921e3a928d5e0f",
+    "url": "/precios-mtg/static/media/dots.b81dabc0.png"
+  },
+  {
+    "revision": "03ef1918e505c3e3471f9369ef7a638f",
+    "url": "/precios-mtg/static/media/nucleo.03ef1918.eot"
+  },
+  {
+    "revision": "5987dd12fea78ce5f97ae601b08ec03c",
+    "url": "/precios-mtg/static/media/nucleo.5987dd12.woff2"
+  },
+  {
+    "revision": "b17a118e13e53558658b681a0ebdad82",
+    "url": "/precios-mtg/static/media/nucleo.b17a118e.ttf"
+  },
+  {
+    "revision": "f0b489a5dbbff08833d21024f9fcbd4e",
+    "url": "/precios-mtg/static/media/nucleo.f0b489a5.woff"
+  },
+  {
+    "revision": "ca25707397fcc982d7dc1f82555f09a0",
+    "url": "/precios-mtg/static/media/square1.ca257073.png"
+  },
+  {
+    "revision": "6619b9792d7d108bb589b51bbd560278",
+    "url": "/precios-mtg/static/media/square2.6619b979.png"
+  },
+  {
+    "revision": "23fa76237f0066f4ffd8f20e77449887",
+    "url": "/precios-mtg/static/media/square3.23fa7623.png"
+  },
+  {
+    "revision": "8fdf9ee20db40e5d1cf778bf99486cb7",
+    "url": "/precios-mtg/static/media/square4.8fdf9ee2.png"
+  },
+  {
+    "revision": "ff004a0e5de54ba9dbd8583fa7e1b3f5",
+    "url": "/precios-mtg/static/media/square5.ff004a0e.png"
+  }
+]);
